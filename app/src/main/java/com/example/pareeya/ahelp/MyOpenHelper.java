d25a7c2b1448d959bf8cdeff170feafcef0d3ce1
pareeya <pop@gmail.com>
2016-10-01 11:40:04 +0700

    add phoenTABLE SQLite finish

--- app/src/main/java/com/example/pareeya/ahelp/MyOpenHelper.java
+++ app/src/main/java/com/example/pareeya/ahelp/MyOpenHelper.java
@@ -12,11 +12,21 @@ public class MyOpenHelper extends SQLiteOpenHelper{
     //Explicit
     public static final String database_name = "aHelp.db";
     private static final int database_version = 1;
+
     private static final String create_user_table = "create table userTABLE (" +
             "_id integer primary key, " +
             "Name text, " +
             "MyPhone text, " +
             "Password text);";
+
+    private static final String create_phone_table = "create table phoneTABLE(" +
+            "_id integer primary key," +
+            "MyPhone text);";
+
+    private String firstString = "insert into phoneTABLE VALUES(null, '');";
+
+
+
 
 
     public MyOpenHelper(Context context) {
@@ -26,6 +36,13 @@ public class MyOpenHelper extends SQLiteOpenHelper{
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
         sqLiteDatabase.execSQL(create_user_table);
+        sqLiteDatabase.execSQL(create_phone_table);
+
+        for (int i=0; i<5; i+=1) {
+            sqLiteDatabase.execSQL(firstString);
+
+        }
+
     }
 
     @Override
