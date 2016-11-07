2fd530341c83067a3a5d9d5ed825b890c4c389a5
pareeya <pop@gmail.com>
2016-11-07 23:32:31 +0700

    7/11/59  tell 1669 finish

--- app/src/main/java/com/example/pareeya/ahelp/HomeActivity.java
+++ app/src/main/java/com/example/pareeya/ahelp/HomeActivity.java
@@ -1,14 +1,17 @@
 package com.example.pareeya.ahelp;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.drawable.AnimationDrawable;
-import android.media.Image;
 import android.net.Uri;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.ImageView;
 import android.widget.Button;
+import android.widget.ImageView;
+
 
 import static com.example.pareeya.ahelp.R.id.bottom;
 import static com.example.pareeya.ahelp.R.id.imageView3;
@@ -41,7 +44,17 @@ public class HomeActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent CallIntent = new Intent(Intent.ACTION_CALL);
-                CallIntent.setData(Uri.parse("tel=1669"));
+                CallIntent.setData(Uri.parse("tel:=1669"));
+                if (ActivityCompat.checkSelfPermission(HomeActivity.this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return;
+                }
                 startActivity(CallIntent);
 
             }
